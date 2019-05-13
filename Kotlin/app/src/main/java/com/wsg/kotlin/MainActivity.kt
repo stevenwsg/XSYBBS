c4d3ffef4336479d28821e2dcaf57568d72ae365
wsg <1305676305@qq.com>
2019-05-13 10:46:43 +0800

    kotlin 版本创建项目

---
+++ Kotlin/app/src/main/java/com/wsg/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wsg.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class dirMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
