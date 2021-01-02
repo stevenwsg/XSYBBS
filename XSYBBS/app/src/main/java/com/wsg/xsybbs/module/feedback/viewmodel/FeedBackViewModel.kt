11461d929950e4f86b4bbf623a13e5b0f524357c
wsg <wangshengguos@gmail.com>
2021-01-02 16:46:16 +0800

    反馈模块将协程请求移动至VM中

--- XSYBBS/app/src/main/java/com/wsg/xsybbs/module/feedback/viewmodel/FeedBackViewModel.kt
+++ XSYBBS/app/src/main/java/com/wsg/xsybbs/module/feedback/viewmodel/FeedBackViewModel.kt
@@ -2,8 +2,14 @@ package com.wsg.xsybbs.module.feedback.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.SaveListener
 import com.wsg.xsybbs.module.feedback.bean.FeedBackResultMessage
-import com.wsg.xsybbs.module.feedback.model.FeedBackModel
+import com.wsg.xsybbs.module.feedback.bean.Feedback
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 /**
  * Created by wsg
  * on         2020/5/5
@@ -12,9 +18,27 @@ import com.wsg.xsybbs.module.feedback.model.FeedBackModel
 class FeedBackViewModel : ViewModel() {
 
     var messageLiveData = MutableLiveData<FeedBackResultMessage>()
-    val mFeedBackModel = FeedBackModel()
+    private var feedBackResultMessage: FeedBackResultMessage? = null
 
     fun getFeedBackMessage(content: String) {
-        mFeedBackModel.feedBack(content, messageLiveData)
+        viewModelScope.launch(Dispatchers.IO) {
+            val feedBack = Feedback(content)
+            feedBack.save(object : SaveListener<String>() {
+                override fun done(p0: String?, p1: BmobException?) {
+                    feedBackResultMessage = if (p1 == null) {
+                        FeedBackResultMessage(
+                            FeedBackResultMessage.CODE_SUCCESS,
+                            FeedBackResultMessage.MESSAGE_SUCCESS
+                        )
+                    } else {
+                        FeedBackResultMessage(
+                            FeedBackResultMessage.CODE_ERROR,
+                            FeedBackResultMessage.MESSAGE_ERROR
+                        )
+                    }
+                    messageLiveData.postValue(feedBackResultMessage)
+                }
+            })
+        }
     }
 }
