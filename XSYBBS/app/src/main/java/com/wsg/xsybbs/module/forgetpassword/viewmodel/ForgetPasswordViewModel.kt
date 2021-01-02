831a5daae29e3771a1ad199db1b69bc1afd1b688
wsg <wangshengguos@gmail.com>
2021-01-02 17:15:26 +0800

    忘记密码将协程移动至VM中

--- XSYBBS/app/src/main/java/com/wsg/xsybbs/module/forgetpassword/viewmodel/ForgetPasswordViewModel.kt
+++ XSYBBS/app/src/main/java/com/wsg/xsybbs/module/forgetpassword/viewmodel/ForgetPasswordViewModel.kt
@@ -2,8 +2,13 @@ package com.wsg.xsybbs.module.forgetpassword.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.UpdateListener
+import com.wsg.xsybbs.bean.User
 import com.wsg.xsybbs.module.forgetpassword.bean.ForgetPasswordResultMessage
-import com.wsg.xsybbs.module.forgetpassword.model.ForgetPasswordModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Created by wsg
@@ -13,9 +18,26 @@ import com.wsg.xsybbs.module.forgetpassword.model.ForgetPasswordModel
 class ForgetPasswordViewModel : ViewModel() {
 
     var liveData = MutableLiveData<ForgetPasswordResultMessage>()
-    val model = ForgetPasswordModel()
+    private var message: ForgetPasswordResultMessage? = null
 
     fun resetPasswordByEmail(email: String) {
-        model.resetPasswordByEmail(email, liveData)
+        viewModelScope.launch(Dispatchers.IO) {
+            User.resetPasswordByEmail(email, object : UpdateListener() {
+                override fun done(p0: BmobException?) {
+                    message = if (p0 == null) {
+                        ForgetPasswordResultMessage(
+                            ForgetPasswordResultMessage.CODE_SUCCESS,
+                            ForgetPasswordResultMessage.MESSAGE_SUCCESS
+                        )
+                    } else {
+                        ForgetPasswordResultMessage(
+                            ForgetPasswordResultMessage.CODE_ERROR,
+                            ForgetPasswordResultMessage.MESSAGE_ERROR
+                        )
+                    }
+                    liveData.postValue(message)
+                }
+            })
+        }
     }
 }
