29be9e9cbe5122a6721613e5cb694f6b3324e15e
wsg <wangshengguos@gmail.com>
2021-03-30 00:37:37 +0800

    集成并初始化Bmob Flutter Sdk

--- flutter_bbs/lib/main.dart
+++ flutter_bbs/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:data_plugin/bmob/bmob.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bbs/post_deatil/utils/constant.dart';
 import 'package:flutter_bbs/post_deatil/view/post_deatil_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +8,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /**
+     * 非加密方式初始化
+     */
+    Bmob.init(
+        "https://api2.bmob.cn", Constant.BMOB_APP_ID, Constant.BMOB_API_KEY);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ flutter_bbs/lib/post_deatil/utils/constant.dart
@@ -0,0 +1,5 @@
+// 存放常量
+class Constant {
+  static const String BMOB_APP_ID = "0734d2ed157a2b96765451adac7c29db";
+  static const String BMOB_API_KEY = "4b6446146ee2c4eb5d6aad33e9827c04";
+}
