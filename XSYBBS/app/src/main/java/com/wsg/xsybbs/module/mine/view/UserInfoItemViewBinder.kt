bd7a242508bfcc65c5baf4303b022d2436f48181
wsg <wangshengguos@gmail.com>
2021-05-30 00:39:40 +0800

    我的页面改版-设置页面点击事件补全

--- XSYBBS/app/src/main/java/com/wsg/xsybbs/module/mine/view/UserInfoItemViewBinder.kt
+++ XSYBBS/app/src/main/java/com/wsg/xsybbs/module/mine/view/UserInfoItemViewBinder.kt
@@ -1,14 +1,29 @@
 package com.wsg.xsybbs.module.mine.view
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.hyphenate.chat.EMClient
+import com.wsg.xsybbs.MainActivity
 import com.wsg.xsybbs.R
+import com.wsg.xsybbs.activity.usercenter.AboutActivity
+import com.wsg.xsybbs.activity.usercenter.UpDateActivity
+import com.wsg.xsybbs.bean.User
+import com.wsg.xsybbs.module.feedback.view.FeedBackActivity
+import com.wsg.xsybbs.module.login.view.LoginActivity
+import com.wsg.xsybbs.module.mine.model.UserSetting
 import com.wsg.xsybbs.module.mine.model.UserSettingInfo
+import com.wsg.xsybbs.module.modifypassword.view.ModifyPasswordActivity
+import com.wsg.xsybbs.module.mynote.view.MyNoteActivity
+import com.wsg.xsybbs.threadpool.MyThreadPool
+import com.wsg.xsybbs.util.SPUtils
+import com.wsg.xsybbs.util.StaticClass
 import me.drakeet.multitype.ItemViewBinder
+import kotlin.system.exitProcess
 
 /**
  * Create by wangshengguo on 2021/5/11.
@@ -40,8 +55,49 @@ class UserInfoItemViewBinder :
     override fun onBindViewHolder(holder: UserInfoViewHolder, item: UserSettingInfo) {
         holder.settingTitle?.text = item.title
         holder.settingIcon?.setImageResource(item.iconId)
-        holder.rootView?.setOnClickListener {
+        holder.rootView?.setOnClickListener { view ->
+            when (item.setting) {
+                UserSetting.MY_NOTE -> {
+                    view.context?.let {
+                        it.startActivity(Intent(it, MyNoteActivity::class.java))
+                    }
+                }
+                UserSetting.MODIFY_PASSWORD -> {
+                    view.context?.let {
+                        it.startActivity(Intent(it, ModifyPasswordActivity::class.java))
+                    }
+                }
+                UserSetting.FEED_BACK -> {
+                    view.context?.let {
+                        it.startActivity(Intent(it, FeedBackActivity::class.java))
+                    }
+                }
+                UserSetting.ABOUT -> {
+                    view.context?.let {
+                        it.startActivity(Intent(it, AboutActivity::class.java))
+                    }
+                }
+                UserSetting.UPDATE -> {
+                    view.context?.let {
+                        it.startActivity(Intent(it, UpDateActivity::class.java))
+                    }
+                }
+                UserSetting.UNREGISTER -> {
+                    User.logOut()
+                    EMClient.getInstance().logout(true)
 
+                    SPUtils.putBoolean(view.context, StaticClass.SHARE_IS_LOGIN, true)
+                    view.context.apply {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                    (view.context as? MainActivity)?.finish()
+                }
+                UserSetting.Exit -> {
+                    //关闭线程池
+                    MyThreadPool.shutDown()
+                    exitProcess(0)
+                }
+            }
         }
     }
 }
