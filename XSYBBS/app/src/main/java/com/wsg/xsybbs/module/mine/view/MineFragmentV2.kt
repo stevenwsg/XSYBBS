31ab209739ebc937299bdbedb13ff11d5f696d37
wsg <1305676305@qq.com>
2021-06-14 15:09:53 +0800

    我的页面引入AppbarLayout, 增强滚动效果

--- XSYBBS/app/src/main/java/com/wsg/xsybbs/module/mine/view/MineFragmentV2.kt
+++ XSYBBS/app/src/main/java/com/wsg/xsybbs/module/mine/view/MineFragmentV2.kt
@@ -1,5 +1,6 @@
 package com.wsg.xsybbs.module.mine.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,50 +8,74 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import cn.bmob.v3.BmobUser
 import com.wsg.xsybbs.R
+import com.wsg.xsybbs.activity.usercenter.MyMessageActivity
 import com.wsg.xsybbs.bean.User
+import com.wsg.xsybbs.databinding.FragmentMineV2Binding
 import com.wsg.xsybbs.module.mine.model.UserSettingCollection
+import com.wsg.xsybbs.module.modifyinfo.view.ModifyUserInfoActivity
+import com.wsg.xsybbs.util.UtilTools
 
 /**
  * Create by wangshengguo on 2021/5/11.
  */
 class MineFragmentV2 : Fragment() {
 
-    private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mineAdapter: MineAdapter
-
-
     companion object {
         const val TAG = "MineFragmentV2"
     }
+
+    private lateinit var mineAdapter: MineAdapter
+
+    private var _binding: FragmentMineV2Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_mine_v2, container, false)
+    ): View {
+        _binding = FragmentMineV2Binding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "onViewCreated")
-        mRecyclerView = view.findViewById(R.id.mineRv)
-        mRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.mineRv.layoutManager = LinearLayoutManager(context)
         mineAdapter = MineAdapter()
 
+        initHeader()
         initData()
+    }
+
+    private fun initHeader() {
+        BmobUser.getCurrentUser(User::class.java)?.let {
+            it.image?.apply {
+                UtilTools.getImage(context, binding.userInfo.profileImage, this)
+            }
+            binding.userInfo.userName.text = it.username
+            val age = it.age.toString() + "岁"
+            binding.userInfo.userAge.text = age
+            binding.userInfo.userGender
+                .setImageResource(if (it.isSex) R.drawable.female else R.drawable.male)
+
+            binding.userInfo.ivMessage.setOnClickListener {
+                context?.startActivity(Intent(context, MyMessageActivity::class.java))
+            }
+            binding.userInfo.ivEdit.setOnClickListener {
+                context?.startActivity(Intent(context, ModifyUserInfoActivity::class.java))
+            }
+        } ?: let {
+            binding.userInfo.header.visibility = View.GONE
+        }
     }
 
     private fun initData() {
         val list = mutableListOf<Any>()
-        BmobUser.getCurrentUser(User::class.java)?.let {
-            list.add(it)
-        }
         list.addAll(UserSettingCollection.getUserSettingCollection())
         mineAdapter.items = list
-        mRecyclerView.adapter = mineAdapter
+        binding.mineRv.adapter = mineAdapter
     }
 }
