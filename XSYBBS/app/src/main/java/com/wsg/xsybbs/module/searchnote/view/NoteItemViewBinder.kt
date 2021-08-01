439c30197ee397a3eb0d244e92558d7ef8877ef2
wsg <1305676305@qq.com>
2021-08-01 11:39:25 +0800

    随机跳转三个版本的帖子详情页面

--- XSYBBS/app/src/main/java/com/wsg/xsybbs/module/searchnote/view/NoteItemViewBinder.kt
+++ XSYBBS/app/src/main/java/com/wsg/xsybbs/module/searchnote/view/NoteItemViewBinder.kt
@@ -1,6 +1,7 @@
 package com.wsg.xsybbs.module.searchnote.view
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +9,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.wsg.xsybbs.R
+import com.wsg.xsybbs.activity.note.NoteDetailActivity
 import com.wsg.xsybbs.bean.Note
 import com.wsg.xsybbs.flutter.MomentDetailActivity
+import com.wsg.xsybbs.module.notedetail.view.NoteDetailV2Activity
 import com.wsg.xsybbs.util.FlutterRoutes
 import com.wsg.xsybbs.util.UtilTools
 import de.hdodenhof.circleimageview.CircleImageView
 import me.drakeet.multitype.ItemViewBinder
+import java.util.Random
 
 /**
  * Created by wsg
@@ -21,6 +25,10 @@ import me.drakeet.multitype.ItemViewBinder
  * function:
  */
 open class NoteItemViewBinder : ItemViewBinder<Note, NoteItemViewBinder.NoteHolder>() {
+
+    companion object {
+        private const val TAG = "NoteItemViewBinder"
+    }
 
     class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -71,20 +79,39 @@ open class NoteItemViewBinder : ItemViewBinder<Note, NoteItemViewBinder.NoteHold
         viewHolder.replaycount?.text = note.replaycount.toString()
 
         viewHolder.containerView?.setOnClickListener {
-//            val intent = Intent(viewHolder.containerView?.context, NoteDetailActivity::class.java)
-//            intent.putExtra("note", note)
-//            viewHolder.containerView?.context?.startActivity(intent)
-
-            val intent = Intent(viewHolder.containerView?.context, MomentDetailActivity::class.java)
-            intent.action = Intent.ACTION_RUN
-            intent.putExtra(
-                FlutterRoutes.ROUTE,
-                "${FlutterRoutes.MOMENT}?${FlutterRoutes.KEY_NOTE_ID}=${note.objectId}"
-            )
-            viewHolder.containerView?.context?.let {
-                it.startActivity(intent)
+            /**
+             * 生成一个随机数， 随机跳转三个版本的帖子详情页面
+             * 1、Flutter版
+             * 2、JAVA版
+             * 3、Kotlin版
+             */
+            val random = Random()
+            when (random.nextInt(3)) {
+                0 -> {
+                    Log.d(TAG, "enter flutter version")
+                    val intent =
+                        Intent(viewHolder.containerView?.context, MomentDetailActivity::class.java)
+                    intent.action = Intent.ACTION_RUN
+                    intent.putExtra(
+                        FlutterRoutes.ROUTE,
+                        "${FlutterRoutes.MOMENT}?${FlutterRoutes.KEY_NOTE_ID}=${note.objectId}"
+                    )
+                    viewHolder.containerView?.context?.startActivity(intent)
+                }
+                1 -> {
+                    Log.d(TAG, "enter Java version")
+                    val intent =
+                        Intent(viewHolder.containerView?.context, NoteDetailActivity::class.java)
+                    intent.putExtra("note", note)
+                    viewHolder.containerView?.context?.startActivity(intent)
+                }
+                2 -> {
+                    Log.d(TAG, "enter kotlin version")
+                    viewHolder.containerView?.context?.let {
+                        NoteDetailV2Activity.launch(it, note)
+                    }
+                }
             }
         }
     }
-
 }
